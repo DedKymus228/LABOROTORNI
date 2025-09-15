fc41d1c477d25163a4e9591bd3cdb4f70bcde8b6
Башкатов Сергій <sbaskatov5@gmail.com>
2025-09-15 18:58:34 +0300

    Add files via upload

---
+++ LAB 1/Task1.cpp
@@ -0,0 +1,24 @@
+//приклад 4
+
+int main() {
+    const short CONST1 = 510;
+    short siVar1;
+    siVar1 = -85;
+    short siVar2 =0x1c;
+
+    const int CONST2 = 0337752;
+    int siVar3;
+    siVar3 = 1024;
+    int siVar4 = 61.37;
+
+    const long CONST3 = 4015642735;
+    long lVar5;
+    lVar5 = 97.8;
+    long lVar6 = 2768;
+
+    const unsigned int CONST4 = 438;
+    unsigned int wVar7;
+    wVar7 = 74234;
+    unsigned int wVar8 = -65;
+}
+
---
+++ LAB 1/Task2.cpp
@@ -0,0 +1,15 @@
+int main() {
+
+    const float CONST1 = 0.0000000002222;
+    float fltVar1;
+    fltVar1 = -61879153264.89;
+    float fltVar2 = 473.5e91;
+
+
+    double dblVar3 = -0.02e-300;
+    double dblVar4;
+    dblVar4 = 365.1e258;
+
+return 0;
+
+}
